757e0e27339ba3cd94e85773d57d0e8ed2d14bb6
Andres Cabada <cm_andres@hotmail.com>
2022-11-16 21:20:35 -0600

    Updated

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ const links = [{
     },
     {
         label: "Week 10 Notes",
-        url: "Week10/index.html"
+        url: "Week10/index2.html"
     },
     {
         label: "Week 11 Notes",
